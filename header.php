a8126134d9555983a844251e32d5131c2ee7c75b
okaken2022 <okaken.address@gmail.com>
2025-03-07 13:41:24 +0900

    fix: header

--- header.php
+++ header.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<meta name="description" content="テキストテキストテキストテキストテキストテキストテキストテキス">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo bloginfo('name'); ?></title>
 	<link rel="icon" href="<?php echo get_template_directory_uri() ?>/img/favicon.ico">
 	<?php wp_head(); ?>
 </head>
